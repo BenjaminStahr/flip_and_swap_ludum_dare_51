0e534ab9f165572172c34da31c08bcaa38ce6f85
BenjaminStahr <BenjaminStahr97@web.de>
2022-10-03 15:41:46 +0200

    build possible

--- Assets/Win.cs
+++ Assets/Win.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Win : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            if (CurrentLevel.CurrentLevelID == 6) 
+            {
+                SceneManager.LoadScene(2, LoadSceneMode.Single);
+            }
             CurrentLevel.CurrentLevelID++;
         }
     }
